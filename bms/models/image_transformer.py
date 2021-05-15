204f568860eab760751ffc9805f0d91dd22d8cc9
jwmccarthy11 <jack.mccarthy@duke.edu>
2021-05-14 20:23:54 -0400

    working on transformer model

--- bms/models/image_transformer.py
+++ bms/models/image_transformer.py
@@ -1,5 +1,7 @@
+import math
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class TextEmbed(nn.Module):
@@ -10,7 +12,8 @@ class TextEmbed(nn.Module):
         self.embed = nn.Embedding(vocab_size, embed_dim)
 
     def forward(self, x):
-        return self.embed(x)
+        x = self.embed(x)
+        return x
 
 
 class ImageEmbed(nn.Module):
@@ -33,3 +36,27 @@ class ImageEmbed(nn.Module):
         x = x.flatten(2)        # (n_samples, embed_dim, n_patches)
         x = x.transpose(1, 2)   # (n_samples, n_patches, embed_dim)
         return x
+
+
+class PositionalEncoder(nn.Module):
+    """Encode position of embedded inputs."""
+
+    def __init__(self, in_features, p_drop=0.1, max_len=5000):
+        super().__init__()
+        self.drop = nn.Dropout(p_drop)
+        pe = torch.zeros(max_len, in_features)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, in_features, 2).float() * (-math.log(10000.0) / in_features)
+        )
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        x = self.drop(x)
+        return x
+
+
